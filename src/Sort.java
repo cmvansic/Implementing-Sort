f08fd7361ade8a4375c736571817dabdbe091672
Caden VanSickle <158854016+cmvansic@users.noreply.github.com>
2024-03-17 15:50:55 -0400

    Wrote the insertionsort method.

--- src/Sort.java
+++ src/Sort.java
@@ -4,12 +4,34 @@ public class Sort implements SortInterface {
 
     @Override
     public void insertionsort(ArrayList<Integer> list, int lowindex, int highindex, boolean reversed) {
+        for (int i = lowindex + 1; i <= highindex; i++) {
+            int key = list.get(i);
+            int j = i - 1;
 
+            /*
+             * Move elements that are greater than the
+             * key to one pos ahead of current pos.
+             */
+            if (!reversed) { //allows for reversed sort when called
+                while (j >= lowindex && list.get(j) > key) {
+                    list.set(j + 1, list.get(j));
+                    j--;
+
+                }
+            } else {
+                while (j >= lowindex && list.get(j) < key) {
+                    list.set(j + 1, list.get(j));
+                    j--;
+                }
+            }
+
+            list.set(j + 1, key);
+        }
     }
 
     @Override
     public void shellsort(ArrayList<Integer> list, int lowindex, int highindex, boolean reversed) {
-//blah
+
     }
 
     @Override
